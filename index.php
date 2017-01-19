9eef52fdde2b5a7eb6703443e2ed490ce0245d31
tmccarten <thomasmccarten@hotmail.com>
2017-01-19 20:17:19 +0000

    rename header.php to head.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ $pageTitle = "Home | Liverpool, Pembroke & Sefton Harriers & Athletics Club";
 $pageDescription = "Official website of Liverpool, Pembroke & Sefton Harriers & Athletics Club. Includes news, about the club and how to become a member.";
 $pageAuthor = "Thomas McCarten";
 
-include ('/assets/includes/header.php');
+include ('/assets/includes/head.php');
 
 ?>
 
