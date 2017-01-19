d6bacc03f17a102067270892f0ae4986cdc0701d
tmccarten <thomasmccarten@hotmail.com>
2017-01-19 20:11:15 +0000

    add header.php and include in index with meta vars

---
+++ assets/includes/header.php
@@ -0,0 +1,21 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+
+  <title><?php echo $pageTitle; ?></title>
+
+  <meta name="description" content="<?php echo $pageDescription; ?>">
+  <meta name="author" content="<?php echo $pageAuthor; ?>">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <link rel="stylesheet" href="/assets/css/normalize.css">
+  <link rel="stylesheet" href="/assets/css/styles.css">
+
+  <!--[if lt IE 9]>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script>
+  <![endif]-->
+</head>
+
+<body>
--- index.php
+++ index.php
@@ -1,21 +1,12 @@
-<!doctype html>
+<?php
 
-<html lang="en">
-<head>
-  <meta charset="utf-8">
+$pageTitle = "Home | Liverpool, Pembroke & Sefton Harriers & Athletics Club";
+$pageDescription = "Official website of Liverpool, Pembroke & Sefton Harriers & Athletics Club. Includes news, about the club and how to become a member.";
+$pageAuthor = "Thomas McCarten";
 
-  <title>New site</title>
-  <meta name="description" content="New site">
-  <meta name="author" content="Tom McCarten">
+include ('/assets/includes/header.php');
 
-  <link rel="stylesheet" href="assets/css/styles.css?v=1.0">
+?>
 
-  <!--[if lt IE 9]>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script>
-  <![endif]-->
-</head>
-
-<body>
-  <script src="js/scripts.js"></script>
 </body>
 </html>
