312b5af776fa13c6a945a24415bf12d4e1f9e86d
tmccarten <thomasmccarten@hotmail.com>
2017-01-19 01:37:52 +0000

    add package.json, gulp file and index.php

---
+++ gulpfile.js
@@ -0,0 +1,41 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync');
+var mmq = require('gulp-merge-media-queries');
+var phpConnect = require('gulp-connect-php');
+var autoPrefixer = require('gulp-autoprefixer');
+
+
+gulp.task('sass', function() {
+  return gulp.src('assets/css/**/*.scss')
+    .pipe(sass()) // Converts Sass to CSS with gulp-sass
+    .pipe(autoPrefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+    .pipe(gulp.dest('assets/css'))
+    .pipe(browserSync.reload({
+      stream: true
+}));
+});
+
+gulp.task('connect-sync', function() {
+  phpConnect.server({}, function (){
+    browserSync({
+      proxy: '127.0.0.1:8000'
+    });
+  });
+});
+
+gulp.task('mmq', ['sass'], function () {
+  gulp.src('assets/css/styles.css')
+    .pipe(mmq({
+      log: true
+    }))
+    .pipe(gulp.dest('assets/css'));
+});
+
+gulp.task('watch', ['connect-sync', 'sass', 'mmq'], function(){
+  gulp.watch('assets/css/**/*.scss', ['sass', 'mmq']);
+  // Other watchers
+});
---
+++ index.php
@@ -0,0 +1,21 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+
+  <title>New site</title>
+  <meta name="description" content="New site">
+  <meta name="author" content="Tom McCarten">
+
+  <link rel="stylesheet" href="assets/css/styles.css?v=1.0">
+
+  <!--[if lt IE 9]>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script>
+  <![endif]-->
+</head>
+
+<body>
+  <script src="js/scripts.js"></script>
+</body>
+</html>
