347a240265ec51b6ee9877085267c927a3b87229
tmccarten <thomasmccarten@hotmail.com>
2017-01-22 20:14:40 +0000

    add nav padding js + responsive font-size to header

--- assets/includes/head.php
+++ assets/includes/head.php
@@ -13,9 +13,12 @@
   <link rel="stylesheet" href="/assets/css/normalize.css">
   <link rel="stylesheet" href="/assets/css/styles.css">
 
+  <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Raleway:400,500" rel="stylesheet">
+
   <!--[if lt IE 9]>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script>
   <![endif]-->
 </head>
 
-<body>  
+<body>
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-// Function to show the nav menu on icon click
+// Function to show the nav menu on menu icon click
 
 var menuIcon = document.querySelector('.menu-icon');
 var nav = document.querySelector('nav');
@@ -9,5 +9,25 @@ menuIcon.addEventListener('click', function() {
 }
   else {
     nav.className = "menu-displayed";
+    headerHeight();
   }
 });
+
+
+// Runs function to get height of fixed header and apply that value to the top padding of the nav
+headerHeight();
+
+window.addEventListener('resize', headerHeight);
+
+function headerHeight() {
+
+  var headerContainer = document.querySelector('.header-container');
+  var navContainer = document.querySelector('.nav-container');
+  var headerContainerHeight = headerContainer.offsetHeight;
+
+  if (nav.className === "menu-not-displayed") {
+  navContainer.style.paddingTop = 0 + "px"; //removes the padding from nav when not in 'mobile' form
+} else {
+  navContainer.style.paddingTop = headerContainerHeight + "px";
+}
+}
