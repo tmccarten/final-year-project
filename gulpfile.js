031a3c10fbc750945a93c4af8858b71d308aa0cf
tmccarten <thomasmccarten@hotmail.com>
2017-01-24 18:49:13 +0000

    change gulp php globs again

--- gulpfile.js
+++ gulpfile.js
@@ -39,7 +39,8 @@
 
         gulp.task('watch', ['connect-sync', 'sass', 'mmq'], function(){
          gulp.watch('assets/css/**/*.scss', ['sass', 'mmq']);
-         gulp.watch('**/*.php', browserSync.reload);
+         gulp.watch('*.php', browserSync.reload);
+         gulp.watch('assets/includes/*.php', browserSync.reload);
          gulp.watch('assets/js/**/*.js', browserSync.reload);
           // Other watchers
         });
