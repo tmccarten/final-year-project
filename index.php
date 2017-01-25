b7b8fadec9b4ee3db16fec1c9d0d502bb2f5e5e7
tmccarten <thomasmccarten@hotmail.com>
2017-01-25 20:18:35 +0000

    add new hero structure + hero buttons

--- index.php
+++ index.php
@@ -13,21 +13,31 @@ include ('/assets/includes/header.php');
   <section class="heroblock">
 
     <div class="heroblock-image">
-      <div class="banner-logo-container">
-        <div class="site-wrapper">
+      <div class="heroblock-image-content">
+        <div class="banner-logo-container">
+          <div class="site-wrapper">
           <h2 class="logo banner">Liverpool, Pembroke
               <span> & Sefton</span>
               <span class="strapline">Harriers & Athletics Club</span>
           </h2>
+          </div>
         </div>
-      </div>
+
+
       <div class="banner-tagline-container">
         <div class="site-wrapper">
           <h3 class="tagline">A quality club in a quality setting.</h3>
           <p class="tagline strapline">Competing with distinction in all types of activity: leagues, county, regional and national championships.</p>
         </div>
       </div>
-      <img src="assets/img/home/image1.jpg" alt="">
+
+      <div class="site-wrapper">
+      <a href="#" class="button">About the club</a>
+      <a href="#" class="button member">Membership</a>
+      </div>
+
+    </div>
+    <img src="assets/img/home/image1.jpg" alt="">
   </div>
 
   </section>
