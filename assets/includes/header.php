fc2d16ab854fd452695c7037ca3f49c8d3377b63
tmccarten <thomasmccarten@hotmail.com>
2017-01-27 18:34:48 +0000

    make buttons and tagline go row at certain width

--- assets/includes/header.php
+++ assets/includes/header.php
@@ -1,18 +1,15 @@
 <div class="header-container">
-  <div class="site-wrapper">
-    <header>
+    <header class="site-wrapper">
         <h1 class="logo">Liverpool, Pembroke
           <span> & Sefton</span>
           <span class="strapline">Harriers & Athletics Club</span>
         </h1>
         <img src="/assets/img/icons/menu-icon.svg" class="menu-parent menu" height="30" width="22" alt="Menu button">
     </header>
-  </div>
 </div>
 
 <div class="nav-container">
-  <div class="site-wrapper">
-    <nav>
+    <nav class="site-wrapper">
       <ul>
         <li><a href=#>Home</a></li>
         <li><a href=#>News</a></li>
@@ -33,5 +30,4 @@
         <li><a href=#>Contact Us</a></li>
       </ul>
     </nav>
-  </div>
 </div>
