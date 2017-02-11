3541f0a26258c21265136ac95a4bd019e92bad92
tmccarten <thomasmccarten@hotmail.com>
2017-02-11 00:35:03 +0000

    add fixtures page

--- fixtures.php
+++ fixtures.php
@@ -20,6 +20,81 @@ include ($_SERVER['DOCUMENT_ROOT'].'/assets/includes/header.php');
 
     </section>
 
+    <section class="contentblock">
+      <div class="site-wrapper">
+        <div class="content-inner">
+          <p>For a more extensive fixture listing please visit <a href="#">Northern Athletics</a></p>
+          <p>Remember Free To Enter events:</p>
+              <p><a href="#">Ormskirk Parkrun</a> every Saturday 9am</p>
+              <p><a href="#">Halewood 5k</a> first Saturday in month 10am</p>
+          </p>
+
+          <h3>Fixtures 16/17</h3>
+
+          <table>
+            <tr><th>Date</th><th>Venue</th><th>Event</th></tr>
+
+            <tr>
+              <td>Sat, 7th Jan.</td>
+              <td>Sherdley Park, St.Helens</td>
+              <td>Merseyside County Cross Country Championships</td>
+            </tr>
+            <tr>
+              <td></td>
+              <td>Witton Park, Blackburn</td>
+              <td>Lancashire County Cross Country Championships</td>
+            </tr>
+            <tr>
+              <td>Sat, 14th Jan.</td>
+              <td>Towneley Park, Burnley</td>
+              <td>Mid-lancs Cross Country League Race 4</td>
+            </tr>
+            <tr>
+              <td>Sat, 28th Jan.</td>
+              <td>Knowsley Safari Park</td>
+              <td>Northern Cross Country Championships</td>
+            </tr>
+            <tr>
+              <td>Sat, 4th Feb.</td>
+              <td>Graves Park, Sheffield</td>
+              <td>British University Championships (BUCS)</td>
+            </tr>
+            <tr>
+              <td>Sun, 5th Feb.</td>
+              <td>Clarkes Gardens, Allerton</td>
+              <td>Liverpool & District Cross Country Championships</td>
+            </tr>
+            <tr>
+              <td>Sat, 11th Feb.</td>
+              <td>Rossall School, Cleveleys</td>
+              <td>Mid-lancs Cross Country League Race 5</td>
+            </tr>
+            <tr>
+              <td>Sat, 4th Mar.</td>
+              <td>Leigh Sports Village</td>
+              <td>Mid-lancs Cross Country League Race 6</td>
+            </tr>
+            <tr>
+              <td>Sat, 11th Mar.</td>
+              <td>Prestwold Hall, Loughbrough</td>
+              <td>Inter Counties Championships</td>
+            </tr>
+            <tr>
+              <td>Sat, 18th Mar.</td>
+              <td>Norwich</td>
+              <td>English Schools Championships</td>
+            </tr>
+            <tr>
+              <td>Sat, 25th Mar.</td>
+              <td>Blackpool</td>
+              <td>Northern Athletics 12 & 6 stage relays</td>
+            </tr>
+          </table>
+        </div>
+
+      </div>
+    </section>
+
     <?php include ($_SERVER['DOCUMENT_ROOT'].'/assets/includes/footer.php'); ?>
 
   </main>
